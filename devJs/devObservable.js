18ef1141c7e1ee8d9cac6115b5ce93f6767a7ee0
Basu Dev Adhikari <53310327+basu-dev@users.noreply.github.com>
2020-10-01 18:55:02 +0545

    observable updated

--- devJs/devObservable.js
+++ devJs/devObservable.js
@@ -1,8 +1,39 @@
-export class devObservable{
+
+ export class devObservable{
     constructor(observer){
         this.observer=observer
     }
-    subscribe({next,error,completed}){
-        this.observer({next,error,completed})
+    subscribe({next,error,complete}){
+        return this.observer({next,error,complete})
     }
-}
+    map(cb=(v)=>v){
+         return new Observable(({next,error,complete})=>{
+             this.subscribe({
+                 next:value=>{
+                    next(cb(value))
+                 }
+             })
+         })
+    }
+    do(cb){
+        return new Observable(({next})=>{
+            this.subscribe({
+                next:(v)=>{
+                    cb(v);
+                    next(v);
+                }
+            })
+        })
+    }
+    filter(cb){
+        return new Observable(({next})=>{
+            this.subscribe({
+                next:(value)=>{
+                    if(cb(value)){
+                        next(value)
+                    }
+                }
+            })
+        })
+    }
+ }
